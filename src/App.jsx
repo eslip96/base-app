7f47d41e8bcceee3f9ebcd2478576dc2e6bef3b8
enoka silipa <eslip96@gmail.com>
2024-08-06 13:12:31 -0600

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Demo from "./components";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Demo />
+    </div>
+  );
 }
 
 export default App;
